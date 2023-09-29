6da68ba74cfaccc77853a75b3ab747b811a86311
krzysiekk22d <klas.club@gmail.com>
2023-09-29 18:59:28 +0200

    Initial Commit

---
+++ calendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  calendar
+//
+//  Created by Krzysztof Czura on 29/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ calendar/calendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  calendarApp.swift
+//  calendar
+//
+//  Created by Krzysztof Czura on 29/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct calendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
